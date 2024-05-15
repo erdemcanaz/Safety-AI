9e9877023a56599ef2dd56833e2a6941185ea3e3
Erdem <erdemcanaz@gmail.com>
2024-05-15 16:49:47 +0300

    .

--- trials/fast_api_simple/main.py
+++ trials/fast_api_simple/main.py
@@ -7,13 +7,6 @@ from fastapi.responses import FileResponse
 
 import uvicorn
 
-
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 with open('users.json') as f:
     users = json.load(f)
 users = users["users"]
